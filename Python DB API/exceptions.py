8f4a205a1ae72e598fccac2025f44c7014566d32
Glayton <gr.martins@outlook.com>
2025-09-04 12:37:02 -0300

    feat(exceptions): módulo de exceções personalidas

---
+++ Python DB API/exceptions.py
@@ -0,0 +1,8 @@
+class ErroDeConexao(Exception):
+    """Excessão personalida para erros de conexão"""
+
+    pass
+
+
+class ErroAoCriarTabelaCliente(Exception):
+    """Excessão personalida para erros na criação da tabela cliente"""
