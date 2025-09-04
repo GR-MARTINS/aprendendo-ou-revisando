cf6e521ae209b61f5fc3f8c7f5715df1317bbee9
Glayton <gr.martins@outlook.com>
2025-09-04 11:46:56 -0300

    feat(criando_tabela): inclusão de exemplo via repositorio

--- Python DB API/02-criando_tabela.py
+++ Python DB API/02-criando_tabela.py
@@ -5,21 +5,50 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 
 
+# Eu estava usando desta forma
+# Isso não é uma boa prática
 def criar_conexao():
     with sqlite3.connect(ROOT_PATH / "clientes.db") as conn:
+        # conn é retornado já fechado.
         return conn
 
 
-# o exemplo de operações podem ser usados conforme descrito abaixo
+# o mais adequado seria assim:
+class ClienteRepository:
+    def __init__(self, driver, string_de_conexao):
+        self.driver = driver
+        self.conexao = driver.connect(string_de_conexao)
+
+    def criar_tabela(self):
+        try:
+            with self.conexao as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    """CREATE TABLE IF NOT EXISTS clientes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(100),
+                    email VARCHAR(150))
+                """
+                )
+        except self.driver.Error:
+            raise
+
+
+# o exemplo de operações pode ser usado conforme descrito abaixo
 # independente do banco utilizado, pois a implementação do connect
 # em todos os bancos, segue os padrões da DB API do python.
 
+# lembrando! A melhor prática é o repositório, especialmente para projetos maiores.
 
-def criar_tabela(conn, cursor):
+
+def criar_tabela(driver, string_de_conexao):
+    conn = driver.connect(string_de_conexao)
+    cursor = conn.cursor()
+
     # mais de uma operação pode ser enviada ao mesmo tempo
     try:
         cursor.execute(
-            """CREATE TABLE clientes (
+            """CREATE TABLE IF NOT EXISTS clientes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nome VARCHAR(100),
                 email VARCHAR(150))
@@ -27,13 +56,14 @@ def criar_tabela(conn, cursor):
         )
         # executa as operações no banco
         conn.commit()
-        return None
 
-    except Exception as erro:
+    except driver.Error:
         # descarta as operações caso haja erro
         conn.rollback()
-        return erro
+        conn.close()
+        raise
 
+    # como não usamos o with no exemplo anterior,
     # se isso fosse feito de outra forma (fora do try except),
     # algumas das operações podem estar corretas e outras não.
     # sem o rollback, as operações que deram certo ainda ficariam
@@ -42,11 +72,23 @@ def criar_tabela(conn, cursor):
     # seriam criadas em duplicidade.
 
 
-conn = criar_conexao()
+# EXEMPLOS DE EXECUÇÃO
 
-erro = criar_tabela(conn, conn.cursor())
+# via função
+string_de_conexao = ROOT_PATH / "cliente.db"
 
-if erro:
-    print(f"Ocorreu um erro ao criar a tabela: {erro}")
-else:
+try:
+    criar_tabela(sqlite3, string_de_conexao)
     print("Tabela criada com sucesso!")
+
+except Exception as erro:
+    print(f"Ocorreu um erro ao criar a tabela: {erro}")
+
+# via repositorio
+repo = ClienteRepository(sqlite3, string_de_conexao)
+
+try:
+    repo.criar_tabela()
+    print("Tabela criada com sucesso!")
+except Exception as erro:
+    print(f"Ocorreu um erro ao criar a tabela: {erro}")
