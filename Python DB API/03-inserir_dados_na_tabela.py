a7d1779801a3e40b1718e72f6c8d68e86e1bc3b8
Glayton <gr.martins@outlook.com>
2025-09-04 13:24:16 -0300

    chore(inserir_dados_na_tabela): ajustes nas funções criar_banco, criar_tabela e inserir_na_tabela + desenvolvimento do ClienteRepository

--- Python DB API/03-inserir_dados_na_tabela.py
+++ Python DB API/03-inserir_dados_na_tabela.py
@@ -1,25 +1,41 @@
 import sqlite3
 from pathlib import Path
-
+from exceptions import ErroDeConexao, ErroAoCriarTabelaCliente, ErroAoInserirCliente
 
 ROOT_PATH = Path(__file__).parent
 
+# Eu estava usando desta forma
+# Isso não é uma boa prática
 
-def criar_conexao():
-    with sqlite3.connect(ROOT_PATH / "clientes.db") as conn:
-        return conn
+# def criar_conexao():
+#     with sqlite3.connect(ROOT_PATH / "clientes.db") as conn:
+#         # conn é retornado já fechado.
+#         return conn
 
 
-# o exemplo de operações podem ser usados conforme descrito abaixo
+# Essa forma é mais adequada do que a anterior
+def criar_conexao(driver, string_de_conexao):
+    try:
+        return driver.connect(string_de_conexao)
+
+    except driver.Error as erro:
+        raise ErroDeConexao(f"Erro ao conectar no banco! {erro}")
+
+
+# o exemplo de operações pode ser usado conforme descrito abaixo
 # independente do banco utilizado, pois a implementação do connect
 # em todos os bancos, segue os padrões da DB API do python.
 
+# lembrando! A melhor prática é o repositório, especialmente para projetos maiores.
 
-def criar_tabela(conn, cursor):
+
+def criar_tabela(conn):
+    cursor = conn.cursor()
+
     # mais de uma operação pode ser enviada ao mesmo tempo
     try:
         cursor.execute(
-            """CREATE TABLE clientes (
+            """CREATE TABLE IF NOT EXISTS clientes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nome VARCHAR(100),
                 email VARCHAR(150))
@@ -27,13 +43,14 @@ def criar_tabela(conn, cursor):
         )
         # executa as operações no banco
         conn.commit()
-        return None
 
     except Exception as erro:
         # descarta as operações caso haja erro
         conn.rollback()
-        return erro
+        conn.close()
+        raise ErroAoCriarTabelaCliente(f"Erro ao criar tabela clientes! {erro}")
 
+    # como não usamos o with no exemplo anterior,
     # se isso fosse feito de outra forma (fora do try except),
     # algumas das operações podem estar corretas e outras não.
     # sem o rollback, as operações que deram certo ainda ficariam
@@ -42,17 +59,9 @@ def criar_tabela(conn, cursor):
     # seriam criadas em duplicidade.
 
 
-conn = criar_conexao()
+def inserir_na_tabela(conn, **kwargs):
+    cursor = conn.cursor()
 
-erro = criar_tabela(conn, conn.cursor())
-
-if erro:
-    print(f"Ocorreu um erro ao criar a tabela: {erro}")
-else:
-    print("Tabela criada com sucesso!")
-
-
-def inserir_na_tabela(conn, cursor, nome, email):
     # extrutura identica a criada na função criar_conexao
     # todas as operações realizadas no banco serão inseridas
     # dessa forma, devido aos motivos comentados anteriormente
@@ -63,18 +72,77 @@ def inserir_na_tabela(conn, cursor, nome, email):
         # da query os place holders "?" ou "%s" (a depender do banco utilizado)
         # e após a query, uma tupla contendo os dados.
         cursor.execute(
-            "INSERT INTO clientes (nome, email) VALUES (?, ?)", (nome, email)
+            "INSERT INTO clientes (nome, email) VALUES (?, ?)",
+            (kwargs.get("nome"), kwargs.get("email")),
         )
         conn.commit()
-        return None
+
     except Exception as erro:
         conn.rollback()
-        return erro
+        raise ErroAoInserirCliente(f"Erro ao inserir cliente! {erro}")
 
 
-erro = inserir_na_tabela(conn, conn.cursor(), "teste", "teste@email.com")
+# Repositório
+# o mais adequado seria assim:
+class ClienteRepository:
+    def __init__(self, driver, string_de_conexao):
+        self.driver = driver
+        try:
+            self.conexao = driver.connect(string_de_conexao)
+        except self.driver.Error as erro:
+            raise ErroDeConexao(f"Erro ao conectar no banco! {erro}")
 
-if erro:
-    print(f"Ocorreu um erro ao inserir dados na tabela: {erro}")
-else:
-    print("Inserção realizada com sucesso!")
+    def criar_tabela(self):
+        try:
+            with self.conexao as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    """CREATE TABLE IF NOT EXISTS clientes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(100),
+                    email VARCHAR(150))
+                """
+                )
+        except self.driver.Error as erro:
+            raise ErroAoCriarTabelaCliente(f"Erro ao criar tabela clientes! {erro}")
+
+    def inserir_na_tabela(self, **kwargs):
+        try:
+            with self.conexao as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO clientes (nome, email) VALUES (?, ?)",
+                    (kwargs.get("nome"), kwargs.get("email")),
+                )
+        except self.driver.Error as erro:
+            raise ErroAoInserirCliente(f"Erro ao inserir cliente! {erro}")
+
+
+# EXEMPLOS DE EXECUÇÃO
+
+string_de_conexao = ROOT_PATH / "cliente.db"
+
+# via função
+try:
+    conn = criar_conexao(sqlite3, string_de_conexao)
+    criar_tabela(conn)
+    print("Tabela criada com sucesso!")
+    dados = {"nome": "teste", "email": "teste@email.com"}
+    inserir_na_tabela(conn, nome=dados.get("nome"), email=dados.get("email"))
+    print(f"Cadastro do cliente {dados.get("nome")} realizado com sucesso!")
+    conn.close()
+
+except Exception as erro:
+    print(f"{erro}")
+
+# via repositorio
+try:
+    repo = ClienteRepository(sqlite3, string_de_conexao)
+    repo.criar_tabela()
+    print("Tabela criada com sucesso!")
+    dados = {"nome": "teste", "email": "teste@email.com"}
+    repo.inserir_na_tabela(nome=dados.get("nome"), email=dados.get("email"))
+    print(f"Cadastro do cliente {dados.get("nome")} realizado com sucesso!")
+
+except Exception as erro:
+    print(f"{erro}")
