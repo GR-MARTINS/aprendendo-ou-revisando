453e90bc794a7e3d2fb1cb7f580891c31b671169
Glayton <gr.martins@outlook.com>
2025-09-27 11:23:05 -0300

    feat(src.app): add and init ma object (Marshmallow instance) and register blueprints user.app and role.app

--- flask-api-blog/src/app.py
+++ flask-api-blog/src/app.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from src.models.base import Base
 from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 bcrypt = Bcrypt()
+ma = Marshmallow()
 
 def create_app(environment=os.environ["FLASK_ENV"]):
     app = Flask(__name__, instance_relative_config=True)
@@ -21,14 +23,15 @@ def create_app(environment=os.environ["FLASK_ENV"]):
     db.init_app(app)
     migrate.init_app(app, db)
     bcrypt.init_app(app)
-
+    ma.init_app(app)
     import src.audits
-    
+
     # Register bluprints
-    from src.blueprints import docs, user
+    from src.blueprints import docs, user, role
 
     app.register_blueprint(docs.json.app)
     app.register_blueprint(docs.ui.app)
     app.register_blueprint(user.app)
+    app.register_blueprint(role.app)
 
     return app
