5f2894cff3bf8961dacc0f1a14a2a2643a366736
Glayton <gr.martins@outlook.com>
2025-09-27 11:18:40 -0300

    feat(src.blueprints.user): add docstrings apispec in create_user function

--- flask-api-blog/src/blueprints/user.py
+++ flask-api-blog/src/blueprints/user.py
@@ -1,10 +1,32 @@
 from flask import Blueprint, request
 from src.repositories.user import UserRepository as repo
+from src.schemas.user import CreateUserSchema
+
+
 app = Blueprint("users", __name__, url_prefix="/users")
+
 
 @app.route("/", methods=["POST"])
 def create_user():
-
+    """Create a new user
+    ---
+    post:
+      tags:
+        - User
+      description: Create a new user
+      summary: create user
+      requestBody:
+        content:
+          application/json:
+            schema: CreateUserSchema
+        required: true
+      responses:
+        201:
+          description: Success Operation
+          content:
+            application/json:
+              schema: MessageSchema
+    """
     data = request.json
     repo.save_user(data)
     return {"message": "user created"}
