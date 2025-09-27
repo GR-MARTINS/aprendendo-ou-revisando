a7777b24621d20636481297d45aa6a4a0691218f
Glayton <gr.martins@outlook.com>
2025-09-27 11:11:33 -0300

    feat(src.blueprints.role): add create_role function + docstring apispec

---
+++ flask-api-blog/src/blueprints/role.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request
+from src.repositories.role import UserRepository as repo
+from src.schemas.role import CreateRoleSchema
+
+app = Blueprint("roles", __name__, url_prefix="/roles")
+
+@app.route("/", methods=["POST"])
+def create_role():
+    """Create a new role
+    ---
+    post:
+      tags:
+        - Role
+      description: Create a new role
+      summary: create role
+      requestBody:
+        content:
+          application/json:
+            schema: CreateRoleSchema
+        description: request data
+        required: true
+      reponses:
+        201:
+          description: Success Operation
+          content:
+            application/json:
+              schema: MessageSchema
+    """
+    data = request.json
+    repo.save_role(data)
+    return {"message": "role created"}
