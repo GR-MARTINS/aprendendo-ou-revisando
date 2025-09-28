1b094be3a5d5fb16ddd6884e169bdc557feb1b89
Glayton <gr.martins@outlook.com>
2025-09-28 11:36:00 -0300

    feat(src.blueprints.docs.json): add blueprints.auth.login in spec path

--- flask-api-blog/src/blueprints/docs/json.py
+++ flask-api-blog/src/blueprints/docs/json.py
@@ -3,6 +3,8 @@ from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from src.schemas.utils import MessageSchema
+from src.schemas.auth import AccessTokenSchema
+
 import src.blueprints as bp
 
 spec = APISpec(
@@ -14,14 +16,11 @@ spec = APISpec(
 )
 
 spec.components.schema("MessageSchema", schema=MessageSchema)
+spec.components.schema("AccessTokenSchema", schema=AccessTokenSchema)
 
 app = Blueprint("api_spec", __name__, url_prefix="/docs")
 
 
 @app.route("/swagger.json")
 def api_spec_json():
-    return (
-        spec.path(view=bp.user.create_user)
-        .path(view=bp.role.create_role)
-        .to_dict()
-        )
+    return spec.path(view=bp.auth.login).path(view=bp.user.create_user).path(view=bp.role.create_role).to_dict()
