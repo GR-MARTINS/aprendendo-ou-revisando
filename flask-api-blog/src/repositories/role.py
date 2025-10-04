a86ac81abdfdf446e7a05df203b5fc359d9ce3ff
Glayton <gr.martins@outlook.com>
2025-10-04 11:36:07 -0300

    feat(src.repositories.role.RoleRepository): add save classmethod

--- flask-api-blog/src/repositories/role.py
+++ flask-api-blog/src/repositories/role.py
@@ -4,9 +4,13 @@ from src.models.role import Role
 class UserRepository:
 
     @classmethod
-    def save_role(cls, data: dict):
-        role = Role(
-            name=data.get("name")
-        )
-        db.session.add(role)
-        db.session.commit()
+    def save(cls, data: dict):
+        try:
+            role = Role(
+                name=data.get("name")
+            )
+            db.session.add(role)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
