1e356635b69e3758799df93f0ff74dfcbc3f4b74
Glayton <gr.martins@outlook.com>
2025-10-04 11:34:43 -0300

    feat(src.repositories.user.UserRepository): add _parse_gender and save classmethods

--- flask-api-blog/src/repositories/user.py
+++ flask-api-blog/src/repositories/user.py
@@ -6,21 +6,35 @@ from src.app import db, bcrypt
 class UserRepository:
 
     @classmethod
-    def save_user(cls, data: dict):
-        user = User(
-            first_name=data.get("first_name"),
-            last_name=data.get("last_name"),
-            username=data.get("username"),
-            email=data.get("email"),
-            password=bcrypt.generate_password_hash(data.get("password")),
-            date_of_birth=datetime.strptime(data.get("date_of_birth"), "%Y-%m-%d"),
-            gender=UserGender.male,
-            updated_at=datetime.now(),
-            role_id=data.get("role_id")
-        )
-        db.session.add(user)
-        db.session.commit()
+    def _parse_gender(data: dict):
+        gender_map = {
+            "male": UserGender.male,
+            "female": UserGender.female,
+            "non-binary": UserGender.non_binary,
+        }
 
+        return gender_map.get(data.get("gender"), UserGender.prefer_not_to_say)
+
+    @classmethod
+    def save(cls, data: dict):
+        try:
+            user = User(
+                first_name=data.get("first_name"),
+                last_name=data.get("last_name"),
+                username=data.get("username"),
+                email=data.get("email"),
+                password=bcrypt.generate_password_hash(data.get("password")),
+                date_of_birth=datetime.strptime(data.get("date_of_birth"), "%Y-%m-%d"),
+                gender=cls._parse_gender(data),
+                updated_at=datetime.now(),
+                role_id=data.get("role_id"),
+            )
+            db.session.add(user)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
+        
     @classmethod
     def get_user_by_username(cls, username: str):
         query = db.select(User).where(User.username == username)
