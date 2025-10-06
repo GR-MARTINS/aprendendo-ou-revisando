16c24cd3a4078706fa4222f0e506b3c49d3b2662
Glayton <gr.martins@outlook.com>
2025-10-06 18:15:36 -0300

    feat(src.blueprints.role): Add get_all_roles and get_role_by_id functions

--- flask-api-blog/src/blueprints/role.py
+++ flask-api-blog/src/blueprints/role.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 from flask import Blueprint, request
+from werkzeug.exceptions import NotFound
 from marshmallow.exceptions import ValidationError
 from src.services.role import RoleService
+from src.schemas.role import RoleSchema, ListRolesSchema
 
 app = Blueprint("roles", __name__, url_prefix="/roles")
 
@@ -18,7 +20,7 @@ def create_role():
       requestBody:
         content:
           application/json:
-            schema: CreateRoleSchema
+            schema: CreateOrUpdateRoleSchema
         description: request data
         required: true
       responses:
@@ -48,6 +50,85 @@ def create_role():
             "message": "Required fields missing or invalid",
             "required_fields": list(error.messages.keys()),
         }, HTTPStatus.BAD_REQUEST
+
+    except Exception as error:
+        return {
+            "message": "Internal Server Error",
+            "exception": type(error).__name__,
+        }, HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@app.route("/")
+def get_all_roles():
+    """Get all roles
+    ---
+    get:
+      tags:
+        - Role
+      description: get all roles
+      summary: get roles
+      responses:
+        200:
+          description: Success Operation
+          content:
+            application/json:
+              schema: ListRolesSchema
+        500:
+          description: Internal Server Error
+          content:
+            application/json:
+              schema: InternalServerErrorSchema
+    """
+    try:
+        roles = RoleService.get_all_roles()
+
+        return ListRolesSchema().dump({"roles": roles}), HTTPStatus.OK
+
+    except Exception as error:
+        return {
+            "message": "Internal Server Error",
+            "exception": type(error).__name__,
+        }, HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+@app.route("/<int:role_id>")
+def get_role_by_id(role_id: int):
+    """Get role by ID
+    ---
+    get:
+      tags:
+        - Role
+      description: get role by id
+      summary: get role
+      parameters:
+        - in: path
+          name: role_id
+          required: true
+          schema:
+            type: integer
+      responses:
+        200:
+          description: Success Operation
+          content:
+            application/json:
+              schema: RoleSchema
+        404:
+          description: Role Not Found
+          content:
+            application/json:
+              schema: MessageSchema
+        500:
+          description: Internal Server Error
+          content:
+            application/json:
+              schema: InternalServerErrorSchema
+    """
+    try:
+        role = RoleService.get_role_by_id(role_id)
+        return role, HTTPStatus.OK
+
+    except NotFound:
+        return {"message": "Role not found"}, HTTPStatus.NOT_FOUND
 
     except Exception as error:
         return {
