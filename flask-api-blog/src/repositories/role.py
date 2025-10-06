2440535bd1633fd886ab4e58dde15d8382eaf905
Glayton <gr.martins@outlook.com>
2025-10-06 18:17:45 -0300

    feat(src.repositories.role): Add get_all_roles and get_role_by_id functions

--- flask-api-blog/src/repositories/role.py
+++ flask-api-blog/src/repositories/role.py
@@ -1,16 +1,35 @@
 from src.app import db
 from src.models.role import Role
+from src.repositories.utils import update
 
-class UserRepository:
+
+class RoleRepository:
 
     @classmethod
     def save(cls, data: dict):
         try:
-            role = Role(
-                name=data.get("name")
-            )
+            role = Role(name=data.get("name"))
             db.session.add(role)
             db.session.commit()
+        except Exception:
+            db.session.rollback()
+            raise
+
+    @classmethod
+    def get_all_roles(cls):
+        try:
+            query = db.select(Role)
+            roles = db.session.execute(query).scalars()
+            return roles
+        except Exception:
+            db.session.rollback()
+            raise
+
+    @classmethod
+    def get_role_by_id(cls, role_id: int):
+        try:
+            role = db.get_or_404(Role, role_id)
+            return role
         except Exception:
             db.session.rollback()
             raise
