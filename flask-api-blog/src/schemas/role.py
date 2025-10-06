686b4bc41d7f9f7750add578ee7ad921412f8a5d
Glayton <gr.martins@outlook.com>
2025-10-06 18:27:00 -0300

    feat(src.schemas.role): Add many schemas

--- flask-api-blog/src/schemas/role.py
+++ flask-api-blog/src/schemas/role.py
@@ -2,7 +2,22 @@ from src.app import ma
 from src.models.role import Role
 
 
-class CreateRoleSchema(ma.SQLAlchemySchema):
-    class Meta: Role
+class RoleSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Role
+
+    id = ma.Integer()
+    name = ma.String()
+    
+
+class ListRolesSchema(ma.SQLAlchemySchema):
+    class meta:
+        model = Role
+    
+    roles = ma.List(ma.Nested(RoleSchema))
+
+class CreateOrUpdateRoleSchema(ma.SQLAlchemySchema):
+    class Meta:
+        Role
 
     name = ma.String(required=True)
