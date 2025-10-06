b3b68c84cb76a8655965d1fc574249d1d42c8818
Glayton <gr.martins@outlook.com>
2025-10-06 18:19:46 -0300

    feat(src.services.role): Add get_all_roles and get_role_by_id functions

--- flask-api-blog/src/services/role.py
+++ flask-api-blog/src/services/role.py
@@ -1,5 +1,5 @@
-from src.repositories.role import UserRepository as repo
-from src.schemas.role import CreateRoleSchema
+from src.repositories.role import RoleRepository as repo
+from src.schemas.role import CreateOrUpdateRoleSchema, RoleSchema
 
 
 class RoleService:
@@ -7,7 +7,27 @@ class RoleService:
     @classmethod
     def create_role(cls, data: dict):
         try:
-            data_validated = CreateRoleSchema().load(data)
+            data_validated = CreateOrUpdateRoleSchema().load(data)
             repo.save(data_validated)
+        except Exception:
+            raise
+
+    @classmethod
+    def get_all_roles(cls):
+        try:
+            schema = RoleSchema(many=True)
+            roles = repo.get_all_roles()
+            roles_validated = schema.dump(roles)
+            return roles_validated
+        except Exception:
+            raise
+
+    @classmethod
+    def get_role_by_id(cls, role_id: int):
+        try:
+            schema = RoleSchema()
+            role = repo.get_role_by_id(role_id)
+            role_validated = schema.dump(role)
+            return role_validated
         except Exception:
             raise
